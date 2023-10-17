5b851f86305354500e809bc66165db0179d2dd39
samy.bencherif <samy.bencherif@olx.com>
2023-10-17 14:15:53 +0200

    add visuals in app

--- nd-disaster-pipeline/app/run.py
+++ nd-disaster-pipeline/app/run.py
@@ -9,11 +9,11 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-#from sklearn.externals import joblib
+# from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -26,6 +26,7 @@ def tokenize(text):
 
     return clean_tokens
 
+
 # load data
 engine = create_engine('sqlite:///../data/project_disaster.db')
 df = pd.read_sql_table('data/project_disaster.db', engine)
@@ -34,20 +35,24 @@ df = pd.read_sql_table('data/project_disaster.db', engine)
 model = joblib.load("../models/classifier_rfc.pkl")
 
 
-
-
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    # plot proportion for each label in class
+    # messages for label 1
+    class_1 = df.drop(['id', 'message', 'original', 'genre'], axis=1).sum()/len(df)
+
+    # messages for label 0
+    class_0 = -1 * (class_1 - 1)
+    class_name = list(class_1.index)
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -66,13 +71,40 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=class_name,
+                    y=class_1,
+                    name='label:1'
+                ),
+                Bar(
+                    x=class_name,
+                    y=class_0,
+                    name='label:0'
+
+                )
+            ],
+
+            'layout': {
+                'title': 'Proportion of each label per class',
+                'yaxis': {
+                    'title': "Proportion"
+                },
+                'xaxis': {
+                    'title': "Class",
+                },
+                'barmode': 'stack'
+            }
         }
+
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -81,13 +113,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
